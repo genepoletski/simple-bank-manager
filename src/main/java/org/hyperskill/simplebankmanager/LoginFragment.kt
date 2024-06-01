40505910447a861c09136c2f43d457ca4174424b
genepoletski <web.developer.poletski@gmail.com>
2024-06-02 11:43:45 +1200

    Stage 1/5 Completed

--- src/main/java/org/hyperskill/simplebankmanager/LoginFragment.kt
+++ src/main/java/org/hyperskill/simplebankmanager/LoginFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+private const val DEFAULT_USERNAME = "Lara"
+private const val DEFAULT_PASSWORD = 1234
 
 /**
  * A simple [Fragment] subclass.
@@ -20,6 +27,13 @@ class LoginFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private var storedUsername = DEFAULT_USERNAME
+    private var storedPassword = DEFAULT_PASSWORD
+
+    private lateinit var loginUsernameEditView: EditText
+    private lateinit var loginPasswordEditView: EditText
+    private lateinit var loginButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +51,61 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        /* TODO: This is a workaround. It's not great that we add AppCompatActivity dependency
+            to the Fragment. Consider using Fragment Result API and Bundle to pass data
+            between Activity and Fragment */
+        val intent = (view.context as AppCompatActivity).intent
+        intent.extras?.let {
+            storedUsername = it.getString("username") ?: storedUsername
+            storedPassword = it.getString("password")?.toInt() ?: storedPassword
+        }
+
+        loginUsernameEditView = view.findViewById(R.id.loginUsername)
+        loginPasswordEditView = view.findViewById(R.id.loginPassword)
+        loginButton = view.findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener { handleLoginButtonClicked() }
+    }
+
+    private val loginUsername: String
+        get() = loginUsernameEditView.text.toString()
+
+    private val loginPassword: String
+        get() = loginPasswordEditView.text.toString()
+
+    private fun handleLoginButtonClicked() {
+        if (isValidLoginUsername() && isValidLoginPassword()) {
+            return onLoginSucces()
+        }
+        onLoginError()
+    }
+
+    private fun isValidLoginUsername() = loginUsername == storedUsername
+
+    private fun isValidLoginPassword(): Boolean {
+        if (loginPassword.isBlank()) {
+            return false
+        }
+
+        if (loginPassword.toInt() != storedPassword) {
+            return false
+        }
+
+        return true
+    }
+
+    private fun onLoginSucces() {
+        Toast.makeText(requireContext(), "logged in", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onLoginError() {
+        Toast.makeText(requireContext(), "invalid credentials", Toast.LENGTH_SHORT).show()
+    }
+
     companion object {
+        // TODO: Research why this needed
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
